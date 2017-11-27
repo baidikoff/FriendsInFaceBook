abeb4b33bfa478b637abbd64b03da46231d0e768
Victoria-Kravets <victoriakravets93@gmail.com>
2017-11-27 18:19:27 +0200

    first commit

---
+++ FriendsInFaceBookTests/FriendsInFaceBookTests.swift
@@ -0,0 +1,36 @@
+//
+//  FriendsInFaceBookTests.swift
+//  FriendsInFaceBookTests
+//
+//  Created by Viktoria on 11/27/17.
+//  Copyright © 2017 Victoria Kravets. All rights reserved.
+//
+
+import XCTest
+@testable import FriendsInFaceBook
+
+class FriendsInFaceBookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
