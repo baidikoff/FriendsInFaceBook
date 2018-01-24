78216625730a749f989f8cb080e293148bd99c97
Victoria-Kravets <victoriakravets93@gmail.com>
2018-01-24 19:47:03 +0200

    - added specs Result map, mapError, bimap

--- FriendsInFaceBookTests/Specs/Library/Types/ResultSpec.swift
+++ FriendsInFaceBookTests/Specs/Library/Types/ResultSpec.swift
@@ -15,16 +15,26 @@ fileprivate enum Error: Swift.Error {
     case fail
     case unknown
 }
+fileprivate enum WrapperError: Swift.Error, Equatable {
+    case fail(Error)
+    
+    public static func ==(lhs: WrapperError, rhs: WrapperError) -> Bool {
+        switch (lhs, rhs) {
+        case (.fail(let lhs), .fail(let rhs)): return lhs == rhs
+        }
+    }
+}
 
-fileprivate typealias Result = FriendsInFaceBook.Result<Int, Error>
+
+fileprivate typealias SpecResult = FriendsInFaceBook.Result<Int, Error>
 
 class ResultSpec: QuickSpec {
     override func spec() {
         let value = 1
-        let valueResult: Result = lift(value)
+        let valueResult: SpecResult = lift(value)
         
         let error = Error.fail
-        let errorResult: Result = lift(error)
+        let errorResult: SpecResult = lift(error)
         
         let defaultError = Error.unknown
         
@@ -52,7 +62,7 @@ class ResultSpec: QuickSpec {
             }
             
             describe("init(value: Value?, error: Error?, `default`: Error)") {
-                let factory = { Result(value: $0, error: $1, default: defaultError) }
+                let factory = { SpecResult(value: $0, error: $1, default: defaultError) }
                 
                 it("should be .success, when Value is non-nil and error is nil") {
                     expect(factory(value, nil)).to(beSuccess(value: value))
@@ -69,21 +79,48 @@ class ResultSpec: QuickSpec {
                     expect(factory(nil,  nil)).to(beFailure(error: defaultError))
                 }
             }
-            
+//            func it shouldTransform<NewValue>(){
+//                
+//            }
             describe("map"){
-                let valueMap: Result = valueResult.map{$0 + 1}
-                let valueMapResult: Result = lift(2)
-                it("value should be equal Optional(2) and error is equal nil"){
-                    expect(valueMapResult.value).to(equal(valueMap.value))
-                    expect(valueMapResult.error).to(beNil())
+                let specError = WrapperError.fail(error)
+                typealias TransformedResult = Result<String, Error>
+                let transform: (SpecResult) -> TransformedResult = { $0.map { "\($0)" } }
+                context("map"){
+                    it("should map value"){
+                        let result: Result = transform(valueResult)
+                        expect(result).to(beSuccess(value: "\(value)"))
+                    }
+                    
+                    it("should map type"){
+                        let result: Result = transform(errorResult)
+                        expect(result).to(beFailure(error: error))
+                    }
                 }
-            }
-            
-            describe("mapError"){
-                let errorMap: Result = errorResult.mapError{_ in return defaultError}
-                it("error should be default error and value is equal nil"){
-                    expect(defaultError).to(equal(errorMap.error))
-                    expect(errorResult.value).to(beNil())
+                
+                context("bimap"){
+                    it("should map value"){
+                        let result: Result<String, WrapperError> = valueResult.bimap(success: { "\($0)" }, failure: { .fail($0) })
+                        expect(result).to(beSuccess(value: "\(value)"))
+                    }
+                    it("should map error"){
+                        let result: Result<String, WrapperError> = errorResult.bimap(success: { "\($0)" }, failure: { .fail($0) })
+                         expect(result).to(beFailure(error: specError))
+                    }
+                }
+                
+                context("mapError"){
+
+                    it("should map error"){
+                        let result: Result<Int, WrapperError> = errorResult.mapError { .fail($0) }
+                        expect(result).to(beFailure(error: specError))
+
+                    }
+
+                    it("should map error type"){
+                        let result: Result<Int, WrapperError> = valueResult.mapError() { .fail($0) }
+                        expect(result).to(beSuccess(value: value))
+                    }
                 }
             }
         }
